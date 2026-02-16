63515fd9976e7d5712a7da55bc84244eeb1d79b3
Azkali <a.ffcc7@gmail.com>
2026-02-16 21:49:47 +0700

    main: fix arguments key name

--- src/llama_supercharged/__main__.py
+++ src/llama_supercharged/__main__.py
@@ -3,11 +3,11 @@ from .llm import LLM
 
 def parser():
     parser = ArgumentParser()
-    parser.add_argument("-j", "--json", type=str, help="JSON file")
+    parser.add_argument("-j", "--json_file", type=str, help="JSON file")
     parser.add_argument("-c", "--cache_dir", type=str, default="cache", help="Cache directory")
     return parser.parse_args()
 
 if __name__ == "__main__":
     args = parser()
-    pt = LLM(json=args.json, cache_dir=args.cache_dir)
+    pt = LLM(json_file=args.json_file, cache_dir=args.cache_dir)
     pt()
